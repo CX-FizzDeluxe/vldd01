7401e475e9e4223cf6ca9988d83b7134d58c9c33
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-15 13:55:10 +0000

    Fri Dec 15 13:55:10 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -1,12 +1,23 @@
 package befaster.solutions;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
 public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
         boolean multiply3 = number % 3 == 0 || String.valueOf(number).contains("3");
         boolean multiply5 = number % 5 == 0 || String.valueOf(number).contains("5");
+        Set<Character> set = new HashSet<>();
+        for (char c : String.valueOf(number).toCharArray()) {
+            set.add(c);
+        }
+        boolean isSameCharater = set.size() == 1;
 
-        if (multiply3 && multiply5) {
+        if (multiply3 && multiply5 && isSameCharater) {
+            return "fizz buzz deluxe";
+        } else if (multiply3 && multiply5) {
             return "fizz buzz";
         } else if (multiply3) {
             return "fizz";
