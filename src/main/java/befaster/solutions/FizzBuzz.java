a3aab22ab6457ce6def56ac06320ee474d7c5b95
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-15 14:28:51 +0000

    Fri Dec 15 14:28:51 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -9,8 +9,8 @@ public class FizzBuzz {
     public static String fizzBuzz(Integer number) {
         boolean multiply3 = number % 3 == 0 || String.valueOf(number).contains("3");
         boolean multiply5 = number % 5 == 0 || String.valueOf(number).contains("5");
-        
-        String result = number % 2 == 1 ? " fake deluxe" : (number % 3 == 0 || number % 5 == 0) ? " deluxe" : "";
+
+        String result = (multiply3 || multiply5) ? number % 2 == 1 ? " fake deluxe" : " deluxe" : "";
 
         if (multiply3 && multiply5) {
             return "fizz".concat(result).concat(" buzz").concat(result);
