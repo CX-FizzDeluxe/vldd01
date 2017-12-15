0577a05806ea051039a18a366e2cf6e1da4ca3e6
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-15 14:42:09 +0000

    Fri Dec 15 14:42:09 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -7,10 +7,10 @@ import java.util.stream.Stream;
 public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
-        boolean multiply3 = number % 3 == 0 ;//|| String.valueOf(number).contains("3");
-        boolean multiply5 = number % 5 == 0; //|| String.valueOf(number).contains("5");
+        boolean multiply3 = number % 3 == 0 || String.valueOf(number).contains("3");
+        boolean multiply5 = number % 5 == 0 || String.valueOf(number).contains("5");
 
-        String deluxeResult = ((multiply3 && String.valueOf(number).contains("3")) || (multiply5 && String.valueOf(number).contains("5"))) ? number % 2 == 1 ? " fake deluxe" : " deluxe" : "";
+        String deluxeResult = ((number % 3 == 0 && String.valueOf(number).contains("3")) || (number % 5 == 0 && String.valueOf(number).contains("5"))) ? number % 2 == 1 ? " fake deluxe" : " deluxe" : "";
 
         if (multiply3 && multiply5) {
             return "fizz".concat(deluxeResult).concat(" buzz").concat(deluxeResult);
--- src/test/java/befaster/solutions/FizzBuzzTest.java
+++ src/test/java/befaster/solutions/FizzBuzzTest.java
@@ -9,11 +9,15 @@ import static org.junit.Assert.*;
  * Created by rafal on 15/12/2017.
  */
 public class FizzBuzzTest {
-/*
-     - {"method":"fizz_buzz","params":[27],"id":"FIZ_R5_006"}, expected: "fizz", got: "fizz fake deluxe"
-            - {"method":"fizz_buzz","params":[129],"id":"FIZ_R5_007"}, expected: "fizz", got: "fizz fake deluxe"
-            - {"method":"fizz_buzz","params":[201],"id":"FIZ_R5_008"}, expected: "fizz", got: "fizz fake deluxe"
-    */
+    /*
+         - {"method":"fizz_buzz","params":[27],"id":"FIZ_R5_006"}, expected: "fizz", got: "fizz fake deluxe"
+                - {"method":"fizz_buzz","params":[129],"id":"FIZ_R5_007"}, expected: "fizz", got: "fizz fake deluxe"
+                - {"method":"fizz_buzz","params":[201],"id":"FIZ_R5_008"}, expected: "fizz", got: "fizz fake deluxe"
+
+                 - {"method":"fizz_buzz","params":[13],"id":"FIZ_R5_010"}, expected: "fizz", got: "13"
+     - {"method":"fizz_buzz","params":[43],"id":"FIZ_R5_011"}, expected: "fizz", got: "43"
+     - {"method":"fizz_buzz","params":[163],"id":"FIZ_R5_012"}, expected: "fizz", got: "163"
+        */
     @Test
     public void fizzBuzzTest() {
         String result = FizzBuzz.fizzBuzz(27);
@@ -23,6 +27,15 @@ public class FizzBuzzTest {
         Assert.assertEquals("fizz", result);
 
         result = FizzBuzz.fizzBuzz(201);
+        Assert.assertEquals("fizz", result);
+
+        result = FizzBuzz.fizzBuzz(13);
+        Assert.assertEquals("fizz", result);
+
+        result = FizzBuzz.fizzBuzz(43);
+        Assert.assertEquals("fizz", result);
+
+        result = FizzBuzz.fizzBuzz(163);
         Assert.assertEquals("fizz", result);
 
     }
